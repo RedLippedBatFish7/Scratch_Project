107980314322663f2c8251426bf5756748023675
Khang Sabre-Nguyen <klsabren.7@outlook.com>
2022-07-01 01:02:17 -0400

    created basic routing and testing Material UI in routes

--- client/App.js
+++ client/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-// import { Route, NavLink, HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Map from './components/map'
+import Dashboard from './pages/Dashboard'
 
 // // import './stylesheets/styles.scss';
 
 // console.log('reading App.jsx');
 
+// <div id='map'>
+//     <Map/>
+// </div> 
+
 // create App
 const App = () => {
 
     return (
-        <div id='map'>
-            <Map/>
-        </div>
-        
+        <>
+          <Router>
+            <div className = 'container'>
+              <Routes>
+                <Route path = '/' element = {<Dashboard/>}/>
+                <Route path = '/login' element = {<Element2/>}/>
+                <Route path = '/register' element = {<Element3/>}/>
+                <Route path = '/map' element = {<Map/>}/>
+              </Routes>
+            </div>
+          </Router>
+        </>
     )
 }
+
 // export App
 export default App;
