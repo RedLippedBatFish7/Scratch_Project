1e0e6a22a6caa4ff407efa614d15fc07359b4e55
SteveFutureSWE <84763472+SteveFutureSWE@users.noreply.github.com>
2022-07-02 10:23:50 -0400

    updating package + beginning FE

--- client/App.js
+++ client/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 // import { Route, NavLink, HashRouter } from 'react-router-dom';
 import Map from './components/map'
-
+import Button from '@material-ui/core/Button'
+import { CssBaseline, makeStyles } from '@material-ui/core';
 // // import './stylesheets/styles.scss';
 
-// console.log('reading App.jsx');
+const useStyles = makeStyles((theme) => ({
+    webmain: {
+        minHeight: '100vh',
+        backgroundColor: '#686de0',
+        fontFamily: 'Roboto',
+        color: 'white'
+    }
+}))
 
 // create App
 const App = () => {
-
+    const classes = useStyles()
     return (
-        <div id='map'>
-            <Map/>
+        
+        <div className={classes.webmain} id='map'>
+            <h1>Test</h1>
+            <Button>Test</Button>
+            <CssBaseline />
         </div>
         
     )
--- client/index.js
+++ client/index.js
@@ -1,16 +1,31 @@
+import { counter } from '@fortawesome/fontawesome-svg-core';
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import map from './components/map'
 
-const app = (
-  <BrowserRouter>
-  <App/>
-  </BrowserRouter>
-);
 
-render(app,document.getElementById('app'))
+const container = document.getElementById('app')
+const root = createRoot(container);
+root.render(<App tab='home'/>)
+
+
+
+
+
+
+//React 17 way old way
+// const app = (
+//   <BrowserRouter>
+//   <Routes>
+//     <Route path="/" element={<App />}>
+//     </Route>
+//   </Routes>
+//   </BrowserRouter>
+// );
+
+// render(app,document.getElementById('app'))
 
 
 // console.log('entered index.js');
