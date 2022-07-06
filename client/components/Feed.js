f9360541a584c720dc0cea83f9cf522b41ecbd5c
SteveFutureSWE <84763472+SteveFutureSWE@users.noreply.github.com>
2022-07-05 23:18:35 -0400

    created zipcode component

--- client/components/Feed.js
+++ client/components/Feed.js
@@ -1,41 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Cooking from '../assets/cooking.jpg';
-import Button from '@material-ui/core/Button';
-import { Stack } from '@mui/material';
-import { Outlet, Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Cooking from "../assets/cooking.jpg";
+import Button from "@material-ui/core/Button";
+import { Stack } from "@mui/material";
+import { Outlet, Link } from "react-router-dom";
+import ZipCodeGrab from "./ZipCodeGrab";
 
 //Styling
 const useStyles = makeStyles((theme) => ({
   body: {
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url(${Cooking})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'none',
-    backgroundColor: 'transparent',
-    padding: '0px 20px',
+    backgroundSize: "cover",
+    backgroundRepeat: "none",
+    backgroundColor: "transparent",
+    padding: "0px 20px",
   },
   heavyFont: {
-    color: 'white',
-    fontWeight: '900',
-    fontSize: '40px',
-    fontFamily: 'Nunito',
+    color: "white",
+    fontWeight: "900",
+    fontSize: "40px",
+    fontFamily: "Nunito",
   },
 }));
 
 export default function Body() {
   //Declare variables and state
   const classes = useStyles();
+  const [ZipCode, setZipCode] = useState(false);
+
+  //Get state of Zipcode
+  useEffect(() => {
+    //FETCH to grab Zipcode data
+    //If Zipcode data exists, render to Zipcode
+    //Else render component asking for Zipcode
+  });
 
   //Return back to DOM
-  return (
-    <div className={classes.body}>
-      <h1 className={classes.heavyFont}>{`This is the feed page!`}</h1>
-      <Outlet />
-    </div>
-  );
+
+  if (ZipCode) {
+  } else {
+    return (
+      <div className={classes.body}>
+        <ZipCodeGrab />
+        <h1 className={classes.heavyFont}>{`Test feed`}</h1>
+        <Outlet />
+      </div>
+    );
+  }
 }
---
+++ client/components/ZipCodeGrab.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Paper } from "@material-ui/core";
+import { TextField } from "@material-ui/core";
+import { Stack } from "@mui/material";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: "20px",
+  },
+}));
+export default function ZipCodeGrab() {
+  const classes = useStyles();
+  //grabs Zipcode from text field
+  const [UserZip, setUserZip] = useState(0);
+  //tracks errors if incorrect zipcode format is entered
+  const [ErrorZip, setErrorZip] = useState(false);
+
+  const submitZipCode = (e) => {
+    const zipRegex = new RegExp("^[0-9]{5}(?:-[0-9]{4})?$");
+    console.log();
+    e.preventDefault();
+    if (zipRegex.test(UserZip)) {
+      console.log("Accepted!");
+      setErrorZip(false);
+      //Post request, send entered field to server
+    } else {
+      setErrorZip(true);
+    }
+  };
+
+  return (
+    <div>
+      <Paper elevation={5} sx={{ p: 2 }} className={classes.paper}>
+        <h1>Which neighborhood are you located in?</h1>
+        <form onSubmit={submitZipCode}>
+          <Stack spacing={2}>
+            <TextField
+              id="outlined-basic"
+              label="Zipcode"
+              variant="outlined"
+              type="number"
+              error={ErrorZip}
+              helperText={
+                ErrorZip == true ? "Please enter a valid Zipcode" : false
+              }
+              onChange={(e) => setUserZip(e.target.value)}
+            />
+            <Button color="primary" variant="contained" type="submit">
+              Submit
+            </Button>
+          </Stack>
+        </form>
+      </Paper>
+    </div>
+  );
+}
